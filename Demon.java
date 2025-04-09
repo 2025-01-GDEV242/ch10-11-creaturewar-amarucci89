bf3d104080f7cff19bb95cf4e170c3faec8264b5
amarucci89 <alexmarucci89@gmail.com>
2025-04-09 01:48:46 -0400

    Added a Demon class

---
+++ Demon.java
@@ -0,0 +1,46 @@
+
+
+/**
+ * The Demon class implements a wrapper for the base Creature class with the following additions
+ * Implements a maximum/minimum strength for the creature type [20/5]
+ * Implements a maximum/minimum hitpoint total for the creature type [25/5]
+ * 
+ * @author Alessandro Marucci
+ * @version 04.09.2025 v1.1
+ */
+public class Demon extends Creature
+{
+    // instance variables - replace the example below with your own
+    private static final int MAX_HUMAN_HP = 25;
+    private static final int MIN_HUMAN_HP = 5;
+    private static final int MAX_HUMAN_STR = 20;
+    private static final int MIN_HUMAN_STR = 5;
+
+    /**
+     * Constructor for objects of class Demon -
+     * Note that the calling class does not need to know anything about the 
+     * requirements of human minimum and maximum values
+     * 
+     * The instantiating class asks for a Demon and the human class is responsible for
+     * return a Demon object with values in the appropriate range
+     * 
+     */
+    public Demon()
+    {
+        // note how the class uses the static randomizer class to
+        // generate the values. This localizes the need to know 
+        // max and min values to this class only
+        // max-min is range of values
+        // range + min ensures that the values don't start at one.
+        super(
+            Randomizer.nextInt(MAX_HUMAN_STR-MIN_HUMAN_STR)+MIN_HUMAN_STR,
+            Randomizer.nextInt(MAX_HUMAN_HP-MIN_HUMAN_HP)+MIN_HUMAN_HP        
+        );
+          
+    }
+    
+    
+    // attack() - not overridden because Humans generate basic damage
+    // takeDamage(int) - not overridden, because Humans take all damage assigned to them
+
+}
